34798cfc168073862b875fe1f2a170fee8df0d22
amrita1520 <amritamaitra1520@gmail.com>
2023-07-10 12:15:45 +0530

    test

---
+++ list.py
@@ -0,0 +1,9 @@
+# find the intersection of two lists.
+def find_intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+intersection = find_intersection(list1, list2)
+print(intersection)  
