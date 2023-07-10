967a68489045460535b0ac259c0599c2934a9ea1
amrita1520 <amritamaitra1520@gmail.com>
2023-07-10 12:17:40 +0530

    test

--- list.py
+++ list.py
@@ -3,7 +3,7 @@ def find_intersection(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 
-list1 = [1, 2, 3, 4, 5]
+list1 = [1, 2, 3, 4, 5,9]
 list2 = [4, 5, 6, 7, 8]
 intersection = find_intersection(list1, list2)
 print(intersection)  
